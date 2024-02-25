0cb5204c99dd0f5bb07d9f6fc3dfe86261373da5
deekshak30 <160143607+deekshak30@users.noreply.github.com>
2024-02-25 12:43:42 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+def madlibs():
+    noun = input("Enter a noun: ")
+    adjective = input("Enter an adjective: ")
+    verb = input("Enter a verb: ")
+
+    madlibs_text = "The " + adjective + " " + noun + " " + verb + " over the lazy dog."
+    print(madlibs_text)
+
+madlibs()
+
