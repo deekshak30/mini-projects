bea9f4ad0301d9241f1d12a73d7fa2a7969be92f
deekshak30 <160143607+deekshak30@users.noreply.github.com>
2024-02-25 12:59:51 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,24 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+target = 12
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found in the array")
